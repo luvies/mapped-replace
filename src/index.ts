33b754eeb2177be7f103e5329764edc177e08bf5
Luke Davies <lkdvs3@gmail.com>
2018-09-19 00:01:56 +0100

    Adjusted formatting

--- src/index.ts
+++ src/index.ts
@@ -9,13 +9,13 @@ import escapeRegex from 'escape-string-regexp';
  * @returns The adjusted string.
  */
 export default function mappedReplace(str: string, rpl: Record<string, string>): string {
-    const keys = Object.keys(rpl);
+  const keys = Object.keys(rpl);
 
-    // if no keys were given, string doesn't need any replacement
-    if (!keys.length) {
-        return str;
-    }
+  // if no keys were given, string doesn't need any replacement
+  if (!keys.length) {
+    return str;
+  }
 
-    const search = new RegExp(keys.map(r => escapeRegex(r)).join('|'), 'g');
-    return str.replace(search, (match) => rpl[match]);
+  const search = new RegExp(keys.map(r => escapeRegex(r)).join('|'), 'g');
+  return str.replace(search, (match) => rpl[match]);
 }
